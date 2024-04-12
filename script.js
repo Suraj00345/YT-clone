07640714fb5023a4d22daf3990c635d1e0e8f53a
Suraj00345 <surajkumarmanna2000@gmail.com>
2024-04-12 18:14:48 +0530

    final commit

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const videoCardContainer = document.querySelector('.video-container');
+const videoCardContainer = document.querySelector(".video-container");
 const BASE_URL = "https://www.googleapis.com/youtube/v3";
 let api_key = "AIzaSyC-o1G1xoIdWvDZPvb97W1JruQ3fojN55g";
 
 let video_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
 
-fetch(video_http + new URLSearchParams({
+fetch(
+  video_http +
+    new URLSearchParams({
       key: api_key,
       part: "snippet",
       chart: "mostPopular",
@@ -17,7 +19,7 @@ fetch(video_http + new URLSearchParams({
   .then((data) => {
     // console.log(data);
     data.items.forEach((item) => {
-        getChannelIcon(item);
+      getChannelIcon(item);
     });
   })
   .catch((err) => console.log(err));
@@ -25,17 +27,22 @@ fetch(video_http + new URLSearchParams({
 //  channel icon
 
 const getChannelIcon = (video_data) => {
-  fetch(channel_http + new URLSearchParams({
+  fetch(
+    channel_http +
+      new URLSearchParams({
         key: api_key,
         part: "snippet",
         id: video_data.snippet.channelId,
-      })).then((res) => res.json())
-     .then((data) => {
-    // console.log(data);
-    video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
-    // console.log(video_data);
-    makeVideoCard(video_data);
-  });
+      })
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      video_data.channelThumbnail =
+        data.items[0].snippet.thumbnails.default.url;
+      // console.log(video_data);
+      makeVideoCard(video_data);
+    });
 };
 
 const makeVideoCard = (data) => {
@@ -63,14 +70,35 @@ const makeVideoCard = (data) => {
     `;
 };
 
+//loader
+
+function loader() {
+  const loader = document.querySelector(".loader");
+  document.appendChild.loader;
+}
+
 // search bar
 
-let searchInput = document.querySelector('.search-bar');
-let searchBtn = document.querySelector('.Search-btn');
+let searchInput = document.querySelector(".search-bar");
+let searchBtn = document.querySelector(".Search-btn");
 let searchLink = "https://www.youtube.com/results?search_query=";
 
-searchBtn.addEventListener('click', (data) => {
-    if(searchInput.value.length){
-        location.href = searchLink + searchInput.value;
+searchBtn.addEventListener("click", (data) => {
+  loader();
+
+  setTimeout(() => {
+    if (searchInput.value.length) {
+      location.href = searchLink + searchInput.value;
     }
+  }, "2000");
 });
+
+
+const buttons = document.querySelectorAll(".tag");
+
+buttons.forEach(button => {
+  button.addEventListener('click', ()=>{
+    console.log(button.innerHTML);
+    location.href = searchLink + button.innerHTML;
+  })
+})
